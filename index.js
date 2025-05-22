3d2d9f7d604a002d34acfa77ee0c85f3ec358154
Elfujiman <tomofujikawa.94@gmail.com>
2025-05-23 07:33:38 +0900

    nodejs

---
+++ index.js
@@ -0,0 +1,18 @@
+index.js
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.github.com');
+    res.json({ message: 'Todo ok', data: response.data });
+  } catch (error) {
+    res.status(500).json({ error: 'Error llamando a GitHub' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
